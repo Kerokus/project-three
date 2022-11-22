d8516efdb12c00773ab20cdc0479bc5b4aa36e11
tyleroverholts <tyoverholts@gmail.com>
2022-11-22 14:52:12 -0700

    Created functionality for get enpoints

--- server/src/app.js
+++ server/src/app.js
@@ -2,13 +2,41 @@ const express = require('express')
 const knex = require('knex')(require('../knexfile.js')['development'])
 const app = express();
 
-
 app.use(express.json())
-// app.use(cookiesParser())
 
-app.get('/missions', (req, res) => {
-  console.log('Get request sent.')
-  res.send('Received')
+//helper functions
+const getRequest = async (endpoint, res) => {
+  // const string = endpoint.replaceAll('"', '');
+  const data = await knex(`${endpoint}`)
+    .select('*')
+    .then(info => info)
+    .catch(err => {
+      console.log(err);
+      return 'Not an object.'
+    })
+
+  if(typeof data === 'object'){
+     res.status(200).send(data)
+  }else{
+    res.status(404).send('There was an error processing your request.')
+  }
+}
+
+//missions endpoint
+app.get('/missions', async (req, res) => {
+  const endpoint = 'mission';
+  const mission = await getRequest(endpoint, res);
 })
 
+//personnel endpoint
+app.get('/personnel', async (req, res) => {
+  const endpoint = 'personnel';
+  const mission = await getRequest(endpoint, res);
+})
+
+//teams endpoint
+app.get('/teams', async (req, res) => {
+  const endpoint = 'teams';
+  const mission = await getRequest(endpoint, res);
+})
 module.exports = app;
