9932c8b0a884cecab55eb37fe5a245e9a86de2c9
tyleroverholts <tyoverholts@gmail.com>
2022-11-22 15:26:50 -0700

    Added deletion functionality for endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -5,38 +5,105 @@ const app = express();
 app.use(express.json())
 
 //helper functions
-const getRequest = async (endpoint, res) => {
-  // const string = endpoint.replaceAll('"', '');
-  const data = await knex(`${endpoint}`)
-    .select('*')
-    .then(info => info)
-    .catch(err => {
-      console.log(err);
-      return 'Not an object.'
-    })
+const getRequest = async (endpoint, res, id) => {
+  let data = null;
+  if(!id){
+    try{
+      data = await knex(`${endpoint}`)
+        .select('*')
+    } catch (e) {
+      console.log(e);
+      res.status(400).send('There was an error processing your request.');
+    }
+  } else {
+    try{
+      data = await knex(`${endpoint}`)
+        .select('*')
+        .where('id', '=', id)
+    } catch (e) {
+      console.log(e);
+      res.status(400).send('There was an error processing your request.');
+    }
+  }
 
-  if(typeof data === 'object'){
-     res.status(200).send(data)
-  }else{
-    res.status(404).send('There was an error processing your request.')
+  if (data.length === 0) {
+    res.status(404).send(`${endpoint} not found`);
+  } else {
+    res.status(200).send(data);
   }
 }
 
 //missions endpoint
 app.get('/missions', async (req, res) => {
-  const endpoint = 'mission';
-  const mission = await getRequest(endpoint, res);
+  const mission = await getRequest('mission', res);
 })
 
 //personnel endpoint
 app.get('/personnel', async (req, res) => {
-  const endpoint = 'personnel';
-  const mission = await getRequest(endpoint, res);
+  const mission = await getRequest('personnel', res);
 })
 
 //teams endpoint
 app.get('/teams', async (req, res) => {
-  const endpoint = 'teams';
-  const mission = await getRequest(endpoint, res);
+  const mission = await getRequest('teams', res);
 })
+
+//mission/:id endpoint
+app.get('/missions/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const mission = await getRequest('mission', res, id);
+})
+
+//personnel/:id endpoint
+app.get('/personnel/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const mission = await getRequest('personnel', res, id);
+})
+
+//teams/:id endpoint
+app.get('/teams/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const mission = await getRequest('teams', res, id);
+})
+
+//delete mission/:id endpoint
+app.delete('/missions/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try{
+    await knex('mission').where('id', id).del();
+    res.status(202).send(`Item with id ${id} successfully deleted.`)
+  } catch (e) {
+    console.log(e);
+    res.status(400).send('There was an error processing your request.');
+  }
+})
+
+//delete personnel/:id endpoint
+app.delete('/personnel/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try{
+    await knex('personnel').where('id', id).del();
+    res.status(202).send(`Item with id ${id} successfully deleted.`)
+  } catch (e) {
+    console.log(e);
+    res.status(400).send('There was an error processing your request.');
+  }
+})
+
+//delete teams/:id endpoint
+app.delete('/teams/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try{
+    await knex('teams').where('id', id).del();
+    res.status(202).send(`Item with id ${id} successfully deleted.`)
+  } catch (e) {
+    console.log(e);
+    res.status(400).send('There was an error processing your request.');
+  }
+})
+
+
+
+
+
 module.exports = app;
